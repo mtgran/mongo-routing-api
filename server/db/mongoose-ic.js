0a15bfc6e646ca6d777b7607c775cc3a8ece03ad
Marcos <mtgran@gmail.com>
2017-07-23 20:24:11 +0200

    Setup app for Heroku try 3

--- server/db/mongoose-ic.js
+++ server/db/mongoose-ic.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/RoutinApp002');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://heroku_dnrbjf4q:cgineh05e27r7udsfd2vbprb2h@ds119223.mlab.com:19223/heroku_dnrbjf4q' || 'mongodb://localhost:27017/RoutinApp002');
 
 // Export the mongoose variable - database - collection
 
