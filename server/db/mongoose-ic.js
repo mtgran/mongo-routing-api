afde8b686b97043414778b446e400d7969bc2783
Marcos <mtgran@gmail.com>
2017-07-23 22:57:32 +0200

    Setup separate development and test environments - 3

--- server/db/mongoose-ic.js
+++ server/db/mongoose-ic.js
@@ -4,11 +4,17 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // mongoose.connect(process.env.MONGODB_URI
-//  || 'mongodb://heroku_dnrbjf4q:cgineh05e27r7udsfd2vbprb2h@ds119223.mlab.com:19223/heroku_dnrbjf4q'
-//  || 'mongodb://localhost:27017/RoutinApp002');
+// || 'mongodb://heroku_dnrbjf4q:cgineh05e27r7udsfd2vbprb2h@ds119223.mlab.com:19223/heroku_dnrbjf4q'
+ // || 'mongodb://localhost:27017/RoutinApp002');
+
+mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect('mongodb://localhost:27017/RoutinApp002');
+
+
 
 ////////////////// mongoose.connect('mongodb://localhost:27017/RoutinApp002', { useMongoClient: true });
-mongoose.connect('mongodb://localhost:27017/RoutinApp002');
+
+
 
 // Export the mongoose variable - database - collection
 
@@ -17,3 +23,5 @@ mongoose.connect('mongodb://localhost:27017/RoutinApp002');
 // };
 //
 module.exports = { mongoose };
+
+// process.env.NODE_ENV
